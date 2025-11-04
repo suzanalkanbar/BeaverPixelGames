514ed8dd91a1e8ad5a3e949288e9748d570fa5e0
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-11-04 12:45:19 +0100

    added template

--- 2dgame/game.js
+++ 2dgame/game.js
@@ -9,69 +9,71 @@ class mainScene {
     this.load.image('coin', '2dgame/assets/coin.png');
   }
   create() {
-    
+
     // This method is called once, just after preload()
     // It will initialize our scene, like the positions of the sprites
     this.player = this.physics.add.sprite(100, 100, 'player');
     this.coin = this.physics.add.sprite(300, 300, 'coin');
     // Store the score in a variable, initialized at 0
-this.score = 0;
+    this.score = 0;
 
-// The style of the text 
-// A lot of options are available, these are the most important ones
-let style = { font: '20px Arial', fill: '#fff' };
+    // The style of the text 
+    // A lot of options are available, these are the most important ones
+    let style = { font: '20px Arial', fill: '#fff' };
 
-// Display the score in the top left corner
-// Parameters: x position, y position, text, style
-this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
-this.arrow = this.input.keyboard.createCursorKeys();
+    // Display the score in the top left corner
+    // Parameters: x position, y position, text, style
+    this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+    this.arrow = this.input.keyboard.createCursorKeys();
 
-    
+
   }
   update() {
     // If the player is overlapping with the coin
-if (this.physics.overlap(this.player, this.coin)) {
-  // Call the new hit() method
-  this.hit();
-}
-    
+    if (this.physics.overlap(this.player, this.coin)) {
+      // Call the new hit() method
+      this.hit();
+    }
+
     // This method is called 60 times per second after create() 
     // It will handle all the game's logic, like movements
     // Handle horizontal movements
-if (this.arrow.right.isDown) {
-  // If the right arrow is pressed, move to the right
-  this.player.x += 3;
-} else if (this.arrow.left.isDown) {
-  // If the left arrow is pressed, move to the left
-  this.player.x -= 3;
-} 
+    if (this.arrow.right.isDown) {
+      // If the right arrow is pressed, move to the right
+      this.player.x += 3;
+    } else if (this.arrow.left.isDown) {
+      // If the left arrow is pressed, move to the left
+      this.player.x -= 3;
+    }
 
-// Do the same for vertical movements
-if (this.arrow.down.isDown) {
-  this.player.y += 3;
-} else if (this.arrow.up.isDown) {
-  this.player.y -= 3;
-} 
+    // Do the same for vertical movements
+    if (this.arrow.down.isDown) {
+      this.player.y += 3;
+    } else if (this.arrow.up.isDown) {
+      this.player.y -= 3;
+    }
   }
+
+  
   hit() {
-  // Change the position x and y of the coin randomly
-  this.coin.x = Phaser.Math.Between(100, 600);
-  this.coin.y = Phaser.Math.Between(100, 300);
+    // Change the position x and y of the coin randomly
+    this.coin.x = Phaser.Math.Between(100, 600);
+    this.coin.y = Phaser.Math.Between(100, 300);
 
-  // Increment the score by 10
-  this.score += 10;
+    // Increment the score by 10
+    this.score += 10;
 
-  // Display the updated score on the screen
-  this.scoreText.setText('score: ' + this.score);
-  // Create a new tween 
-this.tweens.add({
-  targets: this.player, // on the player 
-  duration: 200, // for 200ms 
-  scaleX: 1.2, // that scale vertically by 20% 
-  scaleY: 1.2, // and scale horizontally by 20% 
-  yoyo: true, // at the end, go back to original scale 
-});
-}
+    // Display the updated score on the screen
+    this.scoreText.setText('score: ' + this.score);
+    // Create a new tween 
+    this.tweens.add({
+      targets: this.player, // on the player 
+      duration: 200, // for 200ms 
+      scaleX: 1.2, // that scale vertically by 20% 
+      scaleY: 1.2, // and scale horizontally by 20% 
+      yoyo: true, // at the end, go back to original scale 
+    });
+  }
 }
 
 new Phaser.Game({
