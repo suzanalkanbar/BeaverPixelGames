afb986b445a9883332e8e76eded545c287e9b03e
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-11-04 14:14:56 +0100

    20% pingpong

--- ping_pong/game.js
+++ ping_pong/game.js
@@ -1,4 +1,5 @@
-// created by following https://www.youtube.com/watch?v=f7c9mM7w2fc&t=256s tutorial
+// created by following https://www.youtube.com/playlist?list=PLNwtXgWIx3rg_fv93PMDFfXahzhXxGe5Z tutorial
+// progress: [##        ] 20%
 
 // Create the mainScene
 class mainScene {
@@ -17,23 +18,35 @@ class mainScene {
     It will initialize our scene, like the positions of the sprites
     */
 
-    const ball = this.add.circle(400,250,10, 0xffffff,1) // creates circle
+    const ball = this.add.circle(350, 200, 10, 0xffffff, 1) // creates circle
     this.physics.add.existing(ball) // adds physics to ball
 
-    ball.body.setBounceX(1)
+    ball.body.setBounceX(1) // add bounce
     ball.body.setBounceY(1)
+    ball.body.setCollideWorldBounds() // allows ball to collide with bounds
 
-    ball.body.setCollideWorldBounds() // allows ball to bounce/ collide with bounds
+    ball.body.setVelocity(200, 0) // gives the ball basic velocity
 
-    ball.body.setVelocity(200,200) // gives the ball basic velocity
+    this.paddleLeft = this.add.rectangle(50, 200, 30, 100, 0xffffff, 1)
+    this.physics.add.existing(this.paddleLeft, true)
+
+    this.physics.add.collider(this.paddleLeft, ball) // add collider to ball n paddle
+
+    this.cursors = this.input.keyboard.createCursorKeys() // add keyboard input
   }
   update() {
-
-
     /* 
     This method is called 60 times per second after create() 
     It will handle all the game's logic, like movements
     */
+
+    if (this.cursors.up.isDown) {
+      this.paddleLeft.y -= 3
+      this.paddleLeft.body.updateFromGameObject()
+    } else if (this.cursors.down.isDown) {
+      this.paddleLeft.y += 3
+      this.paddleLeft.body.updateFromGameObject()
+    }
 
     /* Handle horizontal and vertical movements of "this.player"
     if (this.arrow.down.isDown) {
@@ -55,9 +68,10 @@ new Phaser.Game({
   height: 400, // Height of the game in pixels
   backgroundColor: '#7c805f', // The background color (grey)
   scene: mainScene, // The name of the scene we created
-  physics: { default: 'arcade' ,
+  physics: {
+    default: 'arcade',
     arcade: {
-      gravity: {y:0},
+      gravity: { y: 0 },
       debug: true // debug is true.
     }
   }, // The physics engine to use
