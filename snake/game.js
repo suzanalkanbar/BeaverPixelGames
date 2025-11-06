49c139406bc6504c76e00fd7025fab19b2d0b1e4
Julian <J.Kuijs1@student.han.nl>
2025-11-06 13:38:40 +0100

    animated head and acceleration

--- snake/game.js
+++ snake/game.js
@@ -3,31 +3,21 @@ class mainScene {
   // The three methods currently empty
 
   preload() {
-    /*     
-    This method is called once at the beginning
-    It will load all the assets, like sprites and sounds
-    
-    use this line of code to 'preload' your sprites:
-    this.load.image('player', 'game_template/assets/player.png'); 
 
-    give it a name, which is 'player' in this instance
-    don't forget to change the directory (folder/folder/file.png)
-    */
     this.load.image('snakeHead', 'snake/assets/snakehead.png');
+    this.load.spritesheet('snakeSheet', 
+        'snake/assets/snakesheet.png',
+        { frameWidth: 40, frameHeight:40,}
+    );
     this.load.image('snakeBody', 'snake/assets/snakebody.png');
     this.load.image('apple', 'snake/assets/apple.png');
   }
 
   create() {
-    /* 
-    This method is called once, just after preload()
-    It will initialize our scene, like the positions of the sprites
-    
-    use this line of code to 'create' your sprites at a certain position: 
-    this.player = this.physics.add.sprite(100, 100, 'player');
-    */
-    this.velocity = 50
-    this.player = this.physics.add.sprite(300, 200, 'snakeHead');
+    this.velocity = 50 //the speed at which the snake moves
+
+
+    this.player = this.physics.add.sprite(300,200, 'snakeSheet', 0);
     this.body = this.physics.add.sprite(300, 240, 'snakeBody');
     this.apple = this.physics.add.sprite(100, 100, 'apple');
 
@@ -47,56 +37,55 @@ class mainScene {
     // Display the score in the top left corner
     // Parameters: x position, y position, text, style
     this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+    this.speedText = this.add.text(40, 40, this.velocity, style);
   }
 
   update() {
-    /* 
-    This method is called 60 times per second after create() 
-    It will handle all the game's logic, like movements
-    */
-   
-
-   /* VVV Put any other functions and code down here VVV */
 
     if (this.arrow.right.isDown) {
         // If the right arrow is pressed, move to the right
         this.resetVelocity();
         this.player.setVelocityX(this.velocity);
+        this.player.setFrame(1);
         this.body.setVelocityX(this.velocity);
       } else if (this.arrow.left.isDown) {
         // If the left arrow is pressed, move to the left
         this.resetVelocity();
         this.player.setVelocityX(-this.velocity);
+        this.player.setFrame(3);
         this.body.setVelocityX(-this.velocity);
       } 
        // Do the same for vertical movements
       if (this.arrow.down.isDown) {
         this.resetVelocity();
         this.player.setVelocityY(this.velocity);
+        this.player.setFrame(2);
         this.body.setVelocityY(this.velocity);
       } else if (this.arrow.up.isDown) {
         this.resetVelocity();
         this.player.setVelocityY(-this.velocity);
+        this.player.setFrame(0);
         this.body.setVelocityY(-this.velocity);
       } 
 
       if (this.physics.overlap(this.player, this.apple)) {
-        // Call the new hit() method
+        // call the apple eaten function
         this.eaten();
       }
     }
+
+
+       /* VVV Put any other functions and code down here VVV */
 
     eaten(){
       this.apple.x = Phaser.Math.Between(100, 600);
       this.apple.y = Phaser.Math.Between(100, 300);
       this.score += 1
       this.scoreText.setText('score: ' + this.score);
-      if(this.score == 5){
-        this.velocity = 80
-      }else if (this.score == 10){
-        this.velocity = 100
-      }else if (this.score == 15){
-        this.velocity == 140
+      // slowly speeds up the snake until reaching a velocity of 150
+      if(this.score < 29){
+        this.velocity = 50 + (25 * (this.score/7)); 
+        this.speedText.setText(this.velocity);
       }
     }
 
