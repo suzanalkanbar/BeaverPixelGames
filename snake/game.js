753e008bb36a87bf6977e0bbf38ffe64f792a1bd
Julian <J.Kuijs1@student.han.nl>
2025-11-06 14:26:24 +0100

    borders of doom

--- snake/game.js
+++ snake/game.js
@@ -1,3 +1,5 @@
+var borders;
+
 // Create the mainScene
 class mainScene {
   // The three methods currently empty
@@ -9,24 +11,34 @@ class mainScene {
         'snake/assets/snakesheet.png',
         { frameWidth: 40, frameHeight:40,}
     );
+    this.load.image('horborder', 'snake/assets/horizontal border.png');
+    this.load.image('verborder', 'snake/assets/vertical border.png');
     this.load.image('snakeBody', 'snake/assets/snakebody.png');
     this.load.image('apple', 'snake/assets/apple.png');
   }
 
   create() {
+
+    borders = this.physics.add.staticGroup();
+
+    borders.create(380, -33.5, "horborder");
+    borders.create(380, 553.5, "horborder");
+    borders.create(-24, 260, "verborder");
+    borders.create(784, 260, "verborder");
+
     this.velocity = 50 //the speed at which the snake moves
 
 
     this.player = this.physics.add.sprite(300,200, 'snakeSheet', 0);
-    this.body = this.physics.add.sprite(300, 240, 'snakeBody');
+    //this.body = this.physics.add.sprite(300, 240, 'snakeBody');
     this.apple = this.physics.add.sprite(100, 100, 'apple');
 
     this.arrow = this.input.keyboard.createCursorKeys(); 
 
     this.player.setVelocityY(-this.velocity) 
-    this.body.setVelocityY(-this.velocity) 
+    //this.body.setVelocityY(-this.velocity) 
 
-    this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, borders, this.borderHit)
 
     this.score = 2;
 
@@ -47,25 +59,25 @@ class mainScene {
         this.resetVelocity();
         this.player.setVelocityX(this.velocity);
         this.player.setFrame(1);
-        this.body.setVelocityX(this.velocity);
+        //this.body.setVelocityX(this.velocity);
       } else if (this.arrow.left.isDown) {
         // If the left arrow is pressed, move to the left
         this.resetVelocity();
         this.player.setVelocityX(-this.velocity);
         this.player.setFrame(3);
-        this.body.setVelocityX(-this.velocity);
+        //this.body.setVelocityX(-this.velocity);
       } 
        // Do the same for vertical movements
       if (this.arrow.down.isDown) {
         this.resetVelocity();
         this.player.setVelocityY(this.velocity);
         this.player.setFrame(2);
-        this.body.setVelocityY(this.velocity);
+        //this.body.setVelocityY(this.velocity);
       } else if (this.arrow.up.isDown) {
         this.resetVelocity();
         this.player.setVelocityY(-this.velocity);
         this.player.setFrame(0);
-        this.body.setVelocityY(-this.velocity);
+        //this.body.setVelocityY(-this.velocity);
       } 
 
       if (this.physics.overlap(this.player, this.apple)) {
@@ -92,15 +104,19 @@ class mainScene {
     resetVelocity(){
       this.player.setVelocityX(0);
       this.player.setVelocityY(0);
-      this.body.setVelocityX(0);
-      this.body.setVelocityY(0);
+      //this.body.setVelocityX(0);
+      //this.body.setVelocityY(0);
+    }
+
+    borderHit(){
+      alert("game over!");
     }
 }
 
 // Create the game
 new Phaser.Game({
-  width: 700, // Width of the game in pixels
-  height: 400, // Height of the game in pixels
+  width: 760, // Width of the game in pixels
+  height: 520, // Height of the game in pixels
   backgroundColor: '#ecdf7b', // The background color (light yellow)
   scene: mainScene, // The name of the scene we created
   physics: {default: 'arcade',}, // The physics engine to use
