825d75b86c2442bcc0de94d78885dac12a855aff
Julian <J.Kuijs1@student.han.nl>
2025-11-11 12:58:09 +0100

    snake resized

--- snake/game.js
+++ snake/game.js
@@ -10,7 +10,7 @@ class mainScene {
     this.load.image('snakeHead', 'snake/assets/snakehead.png');
     this.load.spritesheet('snakeSheet', 
         'snake/assets/snakesheet.png',
-        { frameWidth: 40, frameHeight:40,}
+        { frameWidth: 20, frameHeight: 20,}
     );
     this.load.image('horborder', 'snake/assets/horizontal border.png');
     this.load.image('verborder', 'snake/assets/vertical border.png');
@@ -27,38 +27,35 @@ class mainScene {
     this.ateApple = this.sound.add('applecrunch');
     this.snakeHiss = this.sound.add('snakeHiss');
 
-    this.borders = this.physics.add.staticGroup();
-
-    this.borders.create(380, -33.5, "horborder");
-    this.borders.create(380, 553.5, "horborder");
-    this.borders.create(-24, 260, "verborder");
-    this.borders.create(784, 260, "verborder");
-
-    this.resetButton = this.physics.add.sprite(380, 260, 'reset')
+    this.resetButton = this.physics.add.sprite(350, 200, 'reset')
     this.resetButton.depth = 1;
 
-    player = this.physics.add.sprite(300,340, 'snakeSheet', 0);
+    player = this.physics.add.sprite(290,350, 'snakeSheet', 0);
     this.body = []
-    Phaser.Utils.Array.Add(this.body, this.physics.add.sprite(300, 380, 'snakeBody'));
-    this.apple = this.physics.add.sprite(100, 100, 'apple');
+    Phaser.Utils.Array.Add(this.body, this.physics.add.sprite(290, 370, 'snakeBody'));
+    this.apple = this.physics.add.sprite(110, 110, 'apple');
 
-    this.xApple = [20, 60, 100, 140, 180, 220, 260, 300, 340, 380, 420, 460, 500, 540, 580, 620, 660, 700, 740]
-    this.yApple = [20, 60, 100, 140, 180, 220, 260, 300, 340, 380, 420, 460, 500]
+    this.xApple = [10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390, 410, 430, 450, 470, 490, 510, 530, 550, 570, 590, 610, 630, 650, 670, 690]
+    this.yApple = [10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390]
 
     this.arrow = this.input.keyboard.createCursorKeys(); 
 
     this.score = 0;
+    this.highScore = 0;
     this.direction = "up";
+    this.distance = 20;
     this.staggered = 0;
 
     // The style of the text 
     // A lot of options are available, these are the most important ones
-    let style = { font: '20px Arial', fill: '#fff' };
+    let style = { font: '15px Arial', fill: '#fff' };
 
     // Display the score in the top left corner
     // Parameters: x position, y position, text, style
     this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
     this.scoreText.depth = 1;
+    this.highScoreText = this.add.text(20, 40, 'highscore: ' + this.highScore, style);
+    this.highScoreText.depth = 1;
   }
 
   update() {
@@ -83,18 +80,18 @@ class mainScene {
       this.yDiff = Math.abs(player.y - this.apple.y) 
 
 
-      if(this.score < 18){
+      if(this.score < 19){
       this.faster = 1* this.score
       }
       
-      if(this.staggered == (28 - this.faster)){
+      if(this.staggered == (25 - this.faster)){
       this.move();
       this.staggered = 0;
       }
 
       this.staggered += 1
 
-      if(this.xDiff < 41 && this.yDiff < 41){
+      if(this.xDiff < 21 && this.yDiff < 21){
         if(this.direction == 'up'){
       player.setFrame(8);
       }else if(this.direction == 'left'){
@@ -116,26 +113,39 @@ class mainScene {
         this.apple.y = this.yApple[Phaser.Math.Between(0, this.yApple.length - 1)];
       }
 
+      if(this.score > this.highScore){
+        this.highScore = this.score
+        this.highScoreText.setText('highscore: ' + this.score);
+      }
+
+      if(this.score > 696){
+        gameover = true;
+      }
+
     }else{ 
-      this.resetButton.visible = true;
-      this.resetButton.setInteractive().on('pointerdown', function(){
-        for(this.i = 0; this.i < this.body.length; this.i++){
-          this.body[this.i].destroy()
-        }
-        player.setFrame(0);
-        this.body = []
-        Phaser.Utils.Array.Add(this.body, this.physics.add.sprite(300, 380, 'snakeBody'));
-        player.x = 300;
-        player.y = 340;
-        this.apple.x = 100;
-        this.apple.y = 100;
-        this.score = 0;
-        this.scoreText.setText('score: ' + this.score);
-        this.resetButton.disableInteractive()
-        this.resetButton.visible = false;
-        this.direction = "up"
-        gameover = false;
-      }, this)
+      if(this.score > 696){
+
+      }else{
+        this.resetButton.visible = true;
+        this.resetButton.setInteractive().on('pointerdown', function(){
+          for(this.i = 0; this.i < this.body.length; this.i++){
+              this.body[this.i].destroy()
+          }
+          player.setFrame(0);
+          this.body = []
+          Phaser.Utils.Array.Add(this.body, this.physics.add.sprite(290, 370, 'snakeBody'));
+          player.x = 290;
+          player.y = 350;
+          this.apple.x = 110;
+          this.apple.y = 110;
+          this.score = 0;
+          this.scoreText.setText('score: ' + this.score);
+          this.resetButton.disableInteractive()
+          this.resetButton.visible = false;
+          this.direction = "up"
+          gameover = false;
+        }, this)
+      }
     }
     }
 
@@ -147,46 +157,46 @@ class mainScene {
       this.previousY = player.y
       if(this.direction == 'up'){
         player.setFrame(0);
-        player.y = player.y - 40
+        player.y = player.y - this.distance
         if(player.y <= 0){
-          player.y += 40
+          player.y += this.distance
           this.borderHit()
         }
       }else if(this.direction == 'down'){
         player.setFrame(2);
-        player.y += 40
-        if(player.y >= 520){
-          player.y = player.y - 40
+        player.y += this.distance
+        if(player.y >= 400){
+          player.y = player.y - this.distance
           this.borderHit()
         }
       }else if(this.direction == 'left'){
         player.setFrame(3);
-        player.x = player.x - 40
+        player.x = player.x - this.distance
         if(player.x <= 0){
-          player.x += 40
+          player.x += this.distance
           this.borderHit()
         }
       }else if(this.direction == 'right'){
         player.setFrame(1);
-        player.x += 40
-        if(player.x >= 760){
-          player.x = player.x - 40
+        player.x += this.distance
+        if(player.x >= 700){
+          player.x = player.x - this.distance
           this.borderHit()
         }
       }
       for(this.i = 0; this.i < this.body.length; this.i++){
         if(player.x == this.body[this.i].x && player.y == this.body[this.i].y){
           if(this.direction == 'up'){
-              player.y += 40
+              player.y += this.distance
               this.borderHit()
           }else if(this.direction == 'down'){
-              player.y = player.y - 40
+              player.y = player.y - this.distance
               this.borderHit()
           }else if(this.direction == 'left'){
-              player.x += 40
+              player.x += this.distance
               this.borderHit()
           }else if(this.direction == 'right'){
-              player.x = player.x - 40
+              player.x = player.x - this.distance
               this.borderHit()
           }
         }
@@ -230,8 +240,8 @@ class mainScene {
 
 // Create the game
 new Phaser.Game({
-  width: 760, // Width of the game in pixels
-  height: 520, // Height of the game in pixels
+  width: 700, // Width of the game in pixels
+  height: 400, // Height of the game in pixels
   backgroundColor: '#ecdf7b', // The background color (light yellow)
   scene: mainScene, // The name of the scene we created
   physics: {default: 'arcade',}, // The physics engine to use
