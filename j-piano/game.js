85f53b5246dc2076e88b40634c9351aaa2534df8
Julian <J.Kuijs1@student.han.nl>
2025-11-13 11:24:30 +0100

    no more launch

--- j-piano/game.js
+++ j-piano/game.js
@@ -29,63 +29,37 @@ class mainScene {
     this.blackY = this.firstKeyY - 36
 
 
-    this.cExtra = this.physics.add.sprite(this.firstKeyX + 24, this.firstKeyY + 130, 'whiteKeyLeft').setInteractive()
+    this.cExtra = this.physics.add.sprite(this.firstKeyX + 12, this.firstKeyY + 65, 'whiteKeyRight').setInteractive()
     this.cKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()
     this.cKey.input.hitArea.setTo(0, 0, 24, 204)
     this.firstKeyX += 38
-    this.dExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 130, 'whiteKeyMiddle').setInteractive().on('pointerdown', function(){
-          //this.dKey.sound.play();
-        }, this);
-    this.dKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive().on('pointerdown', function(){
-          //this.dKey.sound.play();
-        }, this);      
-    this.dKey.body.setSize(10, 130)
-    this.dKey.body.setOffset(14, 0)
+
+    this.dExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 65, 'whiteKeyMiddle').setInteractive()
+    this.dKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()  
+    this.dKey.input.hitArea.setTo(0, 0, 24, 204)
     this.firstKeyX += 38
-    this.eExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 130, 'whiteKeyRight').setInteractive().on('pointerdown', function(){
-          this.eKey.sound.play();
-        }, this);
-    this.eKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.eKey.sound.play();
-        }, this);      
-    this.eKey.body.setSize(24, 204)
-    this.eKey.body.setOffset(14, 0)
+
+    this.eExtra = this.physics.add.sprite(this.firstKeyX - 12, this.firstKeyY + 65, 'whiteKeyLeft').setInteractive()
+    this.eKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()   
     this.firstKeyX += 38
-    this.fExtra = this.physics.add.sprite(this.firstKeyX + 24, this.firstKeyY + 130, 'whiteKeyLeft').setInteractive().on('pointerdown', function(){
-          this.fKey.sound.play();
-        }, this);   
-    this.fKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.fKey.sound.play();
-        }, this);      
-    this.fKey.body.setSize(24, 204, false)
+
+    this.fExtra = this.physics.add.sprite(this.firstKeyX + 12, this.firstKeyY + 65, 'whiteKeyRight').setInteractive()
+    this.fKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()  
+    this.cKey.input.hitArea.setTo(0, 0, 24, 204)
     this.firstKeyX += 38
-    this.gExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 130, 'whiteKeyMiddle').setInteractive().on('pointerdown', function(){
-          this.gKey.sound.play();
-        }, this);   
-    this.gExtra.body.setSize(38, 72, false)
-    this.gKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.gKey.sound.play();
-        }, this);     
-    this.gKey.body.setSize(10, 130)
-    this.gKey.body.setOffset(14, 0)
+
+    this.gExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 65, 'whiteKeyMiddle').setInteractive()
+    this.gKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()
     this.firstKeyX += 38
-    this.aExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 130, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.aKey.sound.play();
-        }, this);   
-    this.aExtra.body.setSize(38, 72, false)
-    this.aKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.aKey.sound.play();
-        }, this);      
+
+    this.aExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 65, 'whiteKeyMiddle').setInteractive() 
+    this.aKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()   
     this.aKey.body.setSize(10, 130)
     this.aKey.body.setOffset(14, 0)
     this.firstKeyX += 38
-    this.bExtra = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 130, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.bKey.sound.play();
-        }, this);   
-    this.bExtra.body.setSize(14, 72, false)
-    this.bKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive().on('pointerdown', function(){
-          this.bKey.sound.play();
-        }, this);      
+
+    this.bExtra = this.physics.add.sprite(this.firstKeyX - 12, this.firstKeyY + 65, 'whiteKeyLeft').setInteractive()
+    this.bKey = this.physics.add.sprite(this.firstKeyX, this.firstKeyY, 'whiteKey').setInteractive()      
     this.bKey.body.setSize(24, 204)
     this.bKey.body.setOffset(14, 0)
 
@@ -95,6 +69,42 @@ class mainScene {
         }, this); 
     this.cKey.on('pointerdown', function(){
           this.cKey.sound.play();
+        }, this);
+    this.dExtra.on('pointerdown', function(){
+          this.dKey.sound.play();
+        }, this); 
+    this.dKey.on('pointerdown', function(){
+          this.dKey.sound.play();
+        }, this);
+    this.eExtra.on('pointerdown', function(){
+          this.eKey.sound.play();
+        }, this); 
+    this.eKey.on('pointerdown', function(){
+          this.eKey.sound.play();
+        }, this);
+    this.fExtra.on('pointerdown', function(){
+          this.fKey.sound.play();
+        }, this); 
+    this.fKey.on('pointerdown', function(){
+          this.fKey.sound.play();
+        }, this);
+    this.gExtra.on('pointerdown', function(){
+          this.gKey.sound.play();
+        }, this); 
+    this.gKey.on('pointerdown', function(){
+          this.gKey.sound.play();
+        }, this);
+    this.aExtra.on('pointerdown', function(){
+          this.aKey.sound.play();
+        }, this); 
+    this.aKey.on('pointerdown', function(){
+          this.aKey.sound.play();
+        }, this);
+    this.bExtra.on('pointerdown', function(){
+          this.bKey.sound.play();
+        }, this); 
+    this.bKey.on('pointerdown', function(){
+          this.bKey.sound.play();
         }, this);
 
     
@@ -107,15 +117,6 @@ class mainScene {
     this.gBlack = this.physics.add.sprite(this.blackX, this.blackY, 'blackKey');
     this.blackX += 38
     this.aBlack = this.physics.add.sprite(this.blackX, this.blackY, 'blackKey');
-
-    
-    //this.cExtra.alpha = 0;
-    this.dExtra.alpha = 0;
-    this.eExtra.alpha = 0;
-    this.fExtra.alpha = 0;
-    this.gExtra.alpha = 0;
-    this.aExtra.alpha = 0;
-    this.bExtra.alpha = 0;
 
 
     this.cKey.sound = this.sound.add('cNote', {volume: 50})
