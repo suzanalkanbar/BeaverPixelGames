1eb2f47b76121e2eaa074c24baa6ef2eeb97be07
Julian <J.Kuijs1@student.han.nl>
2025-11-18 14:12:03 +0100

    added pikmin sounds

--- j-pikmin_Mario/game.js
+++ j-pikmin_Mario/game.js
@@ -4,14 +4,23 @@ class mainScene {
 
   preload() {
 
+    this.load.image('background', 'j-pikmin_Mario/assets/landscape.png')
+
     this.load.spritesheet('walking', 
       'j-pikmin_Mario/assets/pikmin-walk-sheet.png',
       {frameWidth: 66, frameHeigth: 66})
-    this.load.spritesheet('grass', 'j-pikmin_Mario/assets/grass-spritesheet.png',
+    this.load.spritesheet('grass', 
+      'j-pikmin_Mario/assets/grass-spritesheet.png',
       {frameWidth: 30, frameHeigth: 30}
     )
+    this.load.spritesheet('nectar',
+      'j-pikmin_Mario/assets/nectar.png',
+      {frameWidth: 26, frameHeight: 7}
+    )
 
-    this.load.audio('cry', 'j-pikmin_Mario/assets/cry4.mp3')
+    this.load.audio('cry', 'j-pikmin_Mario/assets/cry.mp3')
+    this.load.audio('slurp', 'j-pikmin_Mario/assets/slurp.m4a')
+    this.load.audio('jump', 'j-pikmin_Mario/assets/jump.mp3')
   }
 
   create() {
@@ -21,23 +30,27 @@ class mainScene {
     this.arrow = this.input.keyboard.createCursorKeys();
 
     this.anims.create({
-      key: 'walkRigth',
-      frames: this.anims.generateFrameNumbers('walking', {start: 1, end: 2}),
-      frameRate: 4,
-      repeat: -1
+        key: 'splash',
+        frames: this.anims.generateFrameNumbers('nectar', { frames: [0, 1, 2] }),
+        frameRate: 8,
     });
+
+    this.background = this.add.image(350, 200, 'background')
+    this.background.depth = -1
 
     this.player = this.physics.add.sprite(200, 200, 'walking', 0);
     this.player.body.setSize(26, 66)
     this.player.body.setGravityY(300);
-    this.player.sound = this.sound.add('cry', {volume: 20})
+
+    this.nectar = this.physics.add.sprite(500 ,343, 'nectar', 0)
+    this.nectar.play('splash')
+    this.nectar.sound = this.sound.add('slurp', {volume: 5})
 
     this.grass = this.physics.add.staticGroup()
 
     this.grassX = 15
     this.grassY = 360
     this.spritelength = 30 - 3
-    this.onStart
 
     for(let i = 0; i < 13; i++){
       this.grass.create(this.grassX, this.grassY, 'grass', 13)
@@ -45,9 +58,9 @@ class mainScene {
       this.grassX += this.spritelength
     }
 
-    this.grassX += this.spritelength * 2
+    this.grassX += this.spritelength * 3
 
-    for(let i = 0; i < 13; i++){
+    for(let i = 0; i < 10; i++){
       this.grass.create(this.grassX, this.grassY, 'grass', 13)
       this.grass.create(this.grassX, this.grassY + 27, 'grass', 50)
       this.grassX += this.spritelength
@@ -55,13 +68,14 @@ class mainScene {
 
     this.physics.add.collider(this.player, this.grass)
 
-
   }
+
   update() {
+
 
     if (this.arrow.right.isDown) {
       this.player.setVelocityX(160);
-      if(this.delay < 15){
+       if(this.delay < 15){
         this.player.setFrame(1);
       }else if(this.delay < 30){
         this.player.setFrame(2);
@@ -71,6 +85,7 @@ class mainScene {
       if(this.player.body.touching.down){
         this.delay++
       }
+     
     }else if (this.arrow.left.isDown) {
       this.player.setVelocityX(-160);   
       if(this.delay < 15){
@@ -89,6 +104,7 @@ class mainScene {
     }
 
     if (this.arrow.up.isDown && this.player.body.touching.down) {
+      this.sound.play('jump')
       this.delay = 0
       this.player.setVelocityY(-200);
     }
@@ -96,12 +112,25 @@ class mainScene {
     if(this.player.y >= 400){
       this.player.x = 200
       this.player.y = 200
-      this.player.sound.play()
+      this.nectar.destroy()
+      this.nectar = this.physics.add.sprite(500 ,343, 'nectar', 0)
+      this.nectar.sound = this.sound.add('slurp', {volume: 5})
+      this.nectar.play('splash')
+      this.sound.play('cry')
     }
+
+    if (this.physics.overlap(this.player, this.nectar)) {
+        this.powerUp();
+      }
 
   }
 
   /* VVV Put any other functions and code down here VVV */
+
+  powerUp(){
+    this.nectar.sound.play()
+    this.nectar.destroy()
+  }
 
 }
 
@@ -111,7 +140,7 @@ new Phaser.Game({
   height: 400, // Height of the game in pixels
   backgroundColor: '#5dba2f', // The background color
   scene: mainScene, // The name of the scene we created
-  physics: { default: 'arcade' }, // The physics engine to use
+  physics: { default: 'arcade'}, // The physics engine to use
   parent: 'game', // Create the game inside the <div id="game"> 
 });
 
