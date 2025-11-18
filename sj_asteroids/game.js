08dbe3849e8d616e129710a5d259483d3abfe89e
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-11-18 13:59:33 +0100

    asteroids

--- sj_asteroids/game.js
+++ sj_asteroids/game.js
@@ -9,8 +9,9 @@ class mainScene {
     This method is called once at the beginning
     It will load all the assets, like sprites and sounds
     */
-    this.load.image('player', 'sj_asteroids/assets/bullet_hell_player_ship.png')
+    this.load.spritesheet('player', 'sj_asteroids/assets/bullet_hell_player_ship.png', { frameWidth: 32, frameHeigth: 32 })
     this.load.image('asteroid', 'sj_asteroids/assets/asteroid.png')
+    this.load.image('bullet', 'sj_asteroids/assets/bullet.png')
   }
 
   create() {
@@ -24,23 +25,32 @@ class mainScene {
 
     this.score = 0
 
-    this.player = this.physics.add.sprite(350, 200, 'player')
+    this.player = this.physics.add.sprite(350, 200, 'player', 0)
+    //this.player.body.setSize(32,32)
     this.player.setDrag(0.99)
     this.player.setMaxVelocity(150)
     this.player.setCollideWorldBounds(true)
 
-    this.rockGroup = this.physics.add.group()
-    this.rockArray = []
+    
 
-    for (var i = 0; i < 3; i++) {
-      const rock = this.physics.add.sprite(400 + i*50,200,'asteroid')
+    this.asteroidGroup = this.physics.add.group()
+    this.asteroidArray = []
 
-      this.rockGroup.add(rock,true)
-      this.rockArray.push(rock)
+    // add bunch of rocks 
+    for (var i = 0; i < 10; i++) {
+      var scale = Phaser.Math.FloatBetween(1, 3)
+      const asteroid = this.physics.add.sprite(Phaser.Math.Between(0, width), Phaser.Math.Between(0, height), 'asteroid').setScale(scale)
 
+      this.asteroidGroup.add(asteroid, true)
+      this.asteroidArray.push(asteroid)
     }
 
-    this.scoreText = this.add.text(width - 200, 20,'Score: 0000').setOrigin(0.5)
+    this.physics.add.collider(this.player, this.asteroidGroup, function () {
+      console.log('hit')
+    }
+    )
+
+    this.scoreText = this.add.text(width - 100, 20, 'Score: 0000').setOrigin(0.5)
 
     this.arrow = this.input.keyboard.createCursorKeys()
 
@@ -53,11 +63,12 @@ class mainScene {
 
     if (this.arrow.up.isDown) {
       this.physics.velocityFromRotation(this.player.rotation, 150, this.player.body.acceleration)
-    } else { 
+      this.player.setFrame(1)
+    } else {
       this.player.setAcceleration(0)
+      this.player.setFrame(0)
     }
 
-    // fix hitbox not updating
     if (this.arrow.right.isDown) {
       this.player.setAngularVelocity(300)
     } else if (this.arrow.left.isDown) {
@@ -66,7 +77,7 @@ class mainScene {
       this.player.setAngularVelocity(0)
     }
 
-    this.scoreText.setText('Score: '+ this.score)
+    this.scoreText.setText('Score: ' + this.score)
 
     /* VVV Put any other functions and code down here VVV */
 
