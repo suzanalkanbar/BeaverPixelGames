703031b618a28247ffa058588e4502039ab530d0
Julian <J.Kuijs1@student.han.nl>
2025-11-18 09:45:51 +0100

    pikmin can walk

--- j-pikmin_Mario/game.js
+++ j-pikmin_Mario/game.js
@@ -6,7 +6,7 @@ class mainScene {
 
     this.load.spritesheet('walking', 
       'j-pikmin_Mario/assets/pikmin-walk-sheet.png',
-      {frameWidth: 64, frameHeigth: 66})
+      {frameWidth: 66, frameHeigth: 66})
 
       this.load.spritesheet('grass', 'j-pikmin_Mario/assets/grass-spritesheet.png',
         {frameWidth: 30, frameHeigth: 30}
@@ -14,6 +14,8 @@ class mainScene {
   }
 
   create() {
+
+    this.delay = 0
 
     this.arrow = this.input.keyboard.createCursorKeys();
 
@@ -25,38 +27,67 @@ class mainScene {
     });
 
     this.player = this.physics.add.sprite(200, 200, 'walking', 0);
-    this.player.setCollideWorldBounds(true);
+    this.player.body.setSize(26, 66)
     this.player.body.setGravityY(300);
 
     this.grass = this.physics.add.staticGroup()
 
     this.grassX = 15
+    this.grassY = 360
     this.spritelength = 30 - 3
     this.onStart
 
-    for(let i = 0; i < 26; i++){
-      this.grass.create(this.grassX, 385, 'grass', 13)
+    for(let i = 0; i < 13; i++){
+      this.grass.create(this.grassX, this.grassY, 'grass', 13)
+      this.grass.create(this.grassX, this.grassY + 27, 'grass', 50)
+      this.grassX += this.spritelength
+    }
+
+    this.grassX += this.spritelength * 2
+
+    for(let i = 0; i < 13; i++){
+      this.grass.create(this.grassX, this.grassY, 'grass', 13)
+      this.grass.create(this.grassX, this.grassY + 27, 'grass', 50)
       this.grassX += this.spritelength
     }
 
     this.physics.add.collider(this.player, this.grass)
+
 
   }
   update() {
 
     if (this.arrow.right.isDown) {
       this.player.setVelocityX(160);
-      this.player.setFrame(1);
-      // this.player.play('walkRight', true);
+      if(this.delay < 15){
+        this.player.setFrame(1);
+      }else if(this.delay < 30){
+        this.player.setFrame(2);
+      }else {
+        this.delay = 0
+      }
+      if(this.player.body.touching.down){
+        this.delay++
+      }
     }else if (this.arrow.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.setFrame(3);
+      this.player.setVelocityX(-160);   
+      if(this.delay < 15){
+        this.player.setFrame(3);
+      }else if(this.delay < 30){
+        this.player.setFrame(4);
+      }else {
+        this.delay = 0
+      }
+      if(this.player.body.touching.down){
+        this.delay++
+      }
     }else {
       this.player.setVelocityX(0);
       this.player.setFrame(0);
     }
 
     if (this.arrow.up.isDown && this.player.body.touching.down) {
+      this.delay = 0
       this.player.setVelocityY(-200);
     }
 
