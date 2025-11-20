1a240c36be6fe48552edc88e182b691b34a62678
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-11-20 11:38:49 +0100

    asteroids verder

--- sj_asteroids/game.js
+++ sj_asteroids/game.js
@@ -1,5 +1,12 @@
 // https://freeasteroids.org/ <- inspiratie
 
+// TO DO
+// [ ] add player death if player and asteroid collision
+// [ ] add asteroid death if asteroid and bullet collision
+// [ ] add bullet inactive if asteroid andd bullet collision
+// [ ] add sound of bullet shooting
+
+
 // Create the mainScene
 class mainScene {
   // The three methods currently empty
@@ -12,6 +19,8 @@ class mainScene {
     this.load.spritesheet('player', 'sj_asteroids/assets/bullet_hell_player_ship.png', { frameWidth: 32, frameHeigth: 32 })
     this.load.image('asteroid', 'sj_asteroids/assets/asteroid.png')
     this.load.image('bullet', 'sj_asteroids/assets/bullet.png')
+    this.load.audio('bomb', 'sj_asteroids/assets/8bit_bomb_explosion.wav')
+    this.load.audio('laser', 'sj_asteroids/assets/laser1.wav')
   }
 
   create() {
@@ -26,7 +35,6 @@ class mainScene {
     this.score = 0
 
     this.player = this.physics.add.sprite(350, 200, 'player', 0)
-    //this.player.body.setSize(32,32)
     this.player.setDrag(0.99)
     this.player.setMaxVelocity(150)
     this.player.setCollideWorldBounds(true)
@@ -35,13 +43,13 @@ class mainScene {
     this.asteroidArray = []
 
     // add bunch of rocks 
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < 10; i++) {
       // meteor constructor
       var scale = Phaser.Math.FloatBetween(1, 3)
       const asteroid = this.physics.add.sprite(200, 200, 'asteroid').setScale(scale)
-      asteroid.speed = Phaser.Math.GetSpeed(100,1)
+      asteroid.speed = Phaser.Math.GetSpeed(100, 1)
       asteroid.direction = Phaser.Math.RND.angle()
-      asteroid.angleRotation = Phaser.Math.RND.between(0.8,2.5)
+      asteroid.angleRotation = Phaser.Math.RND.between(0.8, 2.5)
       asteroid.active = true
       asteroid.visible = true
       asteroid.factor = 1
@@ -54,12 +62,25 @@ class mainScene {
       this.asteroidArray.push(asteroid)
     }
 
+    this.bullet = this.physics.add.sprite(200, 200, 'bullet').setScale(0.3)
+    this.bullet.active = false
+    this.bullet.speed = Phaser.Math.GetSpeed(500, 1)
+
+    // this.bulletGroup = this.physics.add.group({
+    //   classType: Phaser.GameObjects.Sprite, // NOT EQUAL TO VIDEO
+    //   maxSize: 1,
+    //   runChildUpdate: true
+    // })
+
     // !!!!!!!!
     this.physics.add.overlap(this.player, this.asteroidGroup, function () {
       console.log('hit')
 
     }
     )
+
+    this.physics.add.overlap(this.bullet, this.asteroidGroup, this.bulletAsteroidCollision, undefined, this)
+    
 
     this.scoreText = this.add.text(width - 100, 20, 'Score: 0000').setOrigin(0.5)
 
@@ -88,6 +109,23 @@ class mainScene {
       this.player.setAngularVelocity(0)
     }
 
+
+    if (this.arrow.space.isDown) {
+
+      if (!this.bullet.active) { // <- UNCOMMENT TO ENABLE ONE BULLET...
+      this.sound.play('laser')
+      this.bullet.setPosition(this.player.x, this.player.y)
+      this.bullet.setActive(true)
+      this.bullet.setVisible(true)
+
+      this.bullet.direction = this.player.rotation
+      this.bullet.rotation = this.bullet.direction
+      }
+    }
+
+    this.bullet.x += Math.cos(this.bullet.direction) * this.bullet.speed * delta
+    this.bullet.y += Math.sin(this.bullet.direction) * this.bullet.speed * delta
+
     for (const asteroid of this.asteroidArray) {
       if (asteroid.active) {
         asteroid.x += asteroid.factor * Math.cos(asteroid.direction) * asteroid.speed * delta
@@ -105,13 +143,20 @@ class mainScene {
         } else if (asteroid.y > 400) {
           asteroid.y = 0
         }
-      } 
+      }
     }
 
     this.scoreText.setText('Score: ' + this.score)
 
-    /* VVV Put any other functions and code down here VVV */
 
+  }
+  /* VVV Put any other functions and code down here VVV */
+  bulletAsteroidCollision(bullet, asteroid) {
+    console.log('bullet hit')
+    bullet.active = false
+    asteroid.destroy()
+    this.score += 10
+    this.sound.play('bomb')
   }
 }
 
