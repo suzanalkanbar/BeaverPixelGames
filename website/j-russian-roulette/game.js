8c6038b9b30e9baa0c793c45631fa9f381f27e88
Julian <J.Kuijs1@student.han.nl>
2025-11-25 14:34:07 +0100

    basics of russian roulette

--- website/j-russian-roulette/game.js
+++ website/j-russian-roulette/game.js
@@ -3,35 +3,95 @@ class mainScene {
   // The three methods currently empty
 
   preload() {
-    this.load.image('test', 'j-russian-roulette/assets/test.png')
+    this.load.spritesheet('restart','j-russian-roulette/assets/Restart roulette.png', 
+      {frameWidth: 50, frameHeight: 23}
+    )
 
-    this.load.audio('jump', 'j-russian-roulette/assets/jump.mp3')
+    this.load.image('shoot','j-russian-roulette/assets/shoot button.png')
+
+    this.load.audio('shot','j-russian-roulette/assets/shot.mp3')
    
   }
 
   create() {
-    this.A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+    this.round = 0
+    this.currentShot = 1
 
-    this.add.image(250, 200, 'test')
+    this.anims.create({
+        key: 'blooddrip',
+        frames: this.anims.generateFrameNumbers('restart', { frames: [0, 1, 2, 3, 4] }),
+        frameRate: 8,
+    });
+
+    this.shootButton = this.physics.add.sprite(350, 350, 'shoot', 0).setOrigin(0.5, 0.5).setScale(2).setInteractive().on('pointerdown', this.shoot, this)
+    this.shootButton.depth = -1
+
+    this.restart = this.physics.add.sprite(350, 350, 'restart', 0).setOrigin(0.5, 0.5).setScale(2).setInteractive().on('pointerdown', this.reset, this)
+    this.restart.depth = -1
+    this.restart.visible = false
+
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+
+    this.style = { font: '70px Arial', fill: '#eeedecff' };
+    this.shotsRoundText = this.add.text(130, 100, '0 out of 6 shot', this.style);
+    this.shotsRoundText.depth = 1;
+
+    this.startRound()
     
-
   }
   update() {
-    if(this.A.isDown){
-      alert('should make noise')
-    this.sound.play('jump')
+
+    if(this.spacebar.isDown){
+      this.sound.play('shot')
     }
   }
 
   /* VVV Put any other functions and code down here VVV */
 
+  startRound(){
+    this.bullet = Phaser.Math.Between(1, 6 - this.round)
+    console.log('here is the bullet: ' + this.bullet)
+  }
+
+  shoot(){
+    console.log(this.currentShot)
+    if(this.currentShot == this.bullet){
+      this.shotsRoundText.setText('you are dead')
+      this.sound.play('shot')
+      this.shootButton.visible = false
+      this.restart.visible = true
+      this.restart.play('blooddrip')
+    }else{
+      this.currentShot++
+      this.shotsRoundText.setText((this.currentShot - 1) + ' out of ' + (6 - this.round) + ' shot')
+    }
+  }
+
+  reset(){
+    if(this.round < 4){
+      this.currentShot = 1
+      this.round++
+      this.shotsRoundText.setText((this.currentShot - 1) + ' out of ' + (6 - this.round) + ' shot')
+      this.restart.visible = false
+      this.shootButton.visible = true
+      this.startRound()
+    }else{
+      this.round = 0
+      console.log('restarted')
+      this.currentShot = 1
+      this.shotsRoundText.setText((this.currentShot - 1) + ' out of ' + (6 - this.round) + ' shot')
+      this.restart.visible = false
+      this.shootButton.visible = true
+      this.startRound()
+    }
+  }
 }
 
 // Create the game
 window.game = new Phaser.Game({
   width: 700, // Width of the game in pixels
   height: 400, // Height of the game in pixels
-  backgroundColor: '#772525ff', // The background color (grey)
+  backgroundColor: '#774625', // The background color
   scene: mainScene, // The name of the scene we created
   physics: { default: 'arcade' }, // The physics engine to use
   parent: 'game', // Create the game inside the <div id="game"> 
