6e01efe01c97c5e6b638e7ea6a33d9f8867c6adc
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-11-25 14:13:02 +0100

    sudoku ieeuuuuww

--- sj_sudoku/game.js
+++ sj_sudoku/game.js
@@ -1,7 +1,6 @@
 // Create the mainScene
 class mainScene {
-  // The three methods currently empty
-
+  // The three methods currently empty 
 
   preload() {
     /*     
@@ -17,11 +16,11 @@ class mainScene {
     It will initialize our scene, like the positions of the sprites
     */
 
+    const whiteColor = 0xffffff
+    const blackColor = 0x000000
+
     // create grid + board:
     if (true) {
-      const whiteColor = 0xffffff
-      const blackColor = 0x000000
-
       this.add.rectangle(350, 200, 360, 360, whiteColor, 1)
       for (var i = 0; i < 10; i++) {
         this.add.line((i * 40) + 170, 200, 0, 0, 0, 361, blackColor, 1).setLineWidth(2)
@@ -30,9 +29,9 @@ class mainScene {
         this.add.line(350, (i * 40) + 20, 361, 0, 0, 0, blackColor, 1).setLineWidth(2)
       }
       const alpha = 0.5
-      this.add.rectangle(230,  80, 116, 116, whiteColor, alpha) // 1
-      this.add.rectangle(350,  80, 116, 116, whiteColor, alpha) // 2
-      this.add.rectangle(470,  80, 116, 116, whiteColor, alpha) // 3
+      this.add.rectangle(230, 80, 116, 116, whiteColor, alpha) // 1
+      this.add.rectangle(350, 80, 116, 116, whiteColor, alpha) // 2
+      this.add.rectangle(470, 80, 116, 116, whiteColor, alpha) // 3
       this.add.rectangle(230, 200, 116, 116, whiteColor, alpha) // 4
       this.add.rectangle(350, 200, 116, 116, whiteColor, alpha) // 5
       this.add.rectangle(470, 200, 116, 116, whiteColor, alpha) // 6
@@ -43,87 +42,110 @@ class mainScene {
 
     // create text:
     if (true) {
-      this.column1Label = this.add.text(190, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column2Label = this.add.text(230, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column3Label = this.add.text(270, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column4Label = this.add.text(310, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column5Label = this.add.text(350, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column6Label = this.add.text(390, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column7Label = this.add.text(430, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column8Label = this.add.text(470, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
-      this.column9Label = this.add.text(510, 200, 'text', {
-        fontSize: 43, color: 'black'
-      }).setOrigin(0.5, 0.5)
+      this.boardLabel = this.add.text(177, 22, '0', { fontSize: 43, color: 'black' }).setOrigin(0, 0)
+
+      // this.Hor1Label = this.add.text(190, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor2Label = this.add.text(230, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor3Label = this.add.text(270, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor4Label = this.add.text(310, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor5Label = this.add.text(350, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor6Label = this.add.text(390, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor7Label = this.add.text(430, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor8Label = this.add.text(470, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
+      // this.Hor9Label = this.add.text(510, 200, 'text', {
+      //   fontSize: 43, color: 'black'
+      // }).setOrigin(0.5, 0.5)
     }
 
     // create rectangles for interaction:
-    if (true) {
-      this.r1c1 = this.add.rectangle(650, 200, 30, 100)
-      this.blub = this.physics.add.sprite(this.firstKeyX, this.firstKeyY + 65, 'whiteKeyMiddle').setInteractive()
-      this.r1c1.input.hitArea.setTo(13, 0, 10, 130)
-      this.physics.add.existing(this.paddleRight, true)
-    }
+    this.board = this.add.rectangle(350, 200, 360, 360, '', 0)
+    this.physics.add.existing(this.board).setInteractive().on('pointerdown', this.getTile, this)
+
 
   }
   update() {
-    var solution = {
-      column1: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column2: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column3: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column4: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column5: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column6: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column7: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column8: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column9: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    this.solution = {
+      Hor1: [1, 2, 7, 8, 6, 9, 5, 3, 4],
+      Hor2: [9, 4, 3, 5, 1, 7, 8, 2, 6],
+      Hor3: [5, 6, 8, 4, 3, 2, 9, 7, 1],
+      Hor4: [8, 3, 5, 1, 9, 4, 7, 6, 2],
+      Hor5: [7, 1, 4, 6, 2, 8, 3, 5, 9],
+      Hor6: [6, 9, 2, 7, 5, 3, 4, 1, 8],
+      Hor7: [2, 7, 1, 9, 4, 5, 6, 8, 3],
+      Hor8: [4, 8, 6, 3, 7, 1, 2, 9, 5],
+      Hor9: [3, 5, 9, 2, 8, 6, 1, 4, 7]
     }
 
-    var gameBoard = { // column1[0] = 3 and not 1
-      column1: [3, 2, 3, 4, 5, 6, 7, 8, 9],
-      column2: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column3: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column4: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column5: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column6: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column7: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column8: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      column9: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    this.gameBoard = {
+      Hor1: [1, 0, 7, 8, 6, 0, 5, 0, 4],
+      Hor2: [0, 0, 3, 5, 0, 7, 0, 0, 0],
+      Hor3: [5, 0, 8, 0, 0, 0, 0, 0, 0],
+      Hor4: [8, 0, 0, 0, 0, 0, 7, 6, 0],
+      Hor5: [0, 0, 4, 0, 2, 8, 3, 0, 9],
+      Hor6: [6, 9, 2, 7, 0, 3, 0, 0, 8],
+      Hor7: [0, 7, 1, 9, 0, 5, 6, 0, 3],
+      Hor8: [0, 0, 6, 0, 0, 0, 0, 9, 5],
+      Hor9: [3, 0, 9, 2, 8, 0, 1, 4, 7]
     }
 
-    this.drawBoard(gameBoard)
+    this.drawBoard(this.gameBoard)
 
-    this.checkSolution()
   }
 
   /* VVV Put any other functions and code down here VVV */
-  drawBoard(board) {
-    this.column1Label.setText(board.column1)
-    this.column2Label.setText(board.column2)
-    this.column3Label.setText(board.column3)
-    this.column4Label.setText(board.column4)
-    this.column5Label.setText(board.column5)
-    this.column6Label.setText(board.column6)
-    this.column7Label.setText(board.column7)
-    this.column8Label.setText(board.column8)
-    this.column9Label.setText(board.column9)
+
+  drawBoard(board) { // draw/ update board text
+    var horNames = ["Hor1", "Hor2", "Hor3", "Hor4", "Hor5", "Hor6", "Hor7", "Hor8", "Hor9"]
+
+    for(var i = 0; i < 81; i ++) {
+      var currentIndex = Math.floor(i / 9)
+      var text = ""
+      text = text + this.gameBoard[horNames[currentIndex]][currentIndex]
+    }
+    this.boardLabel.setText(text)
+
+    // this.Hor1Label.setText(board.Hor1)
+    // this.Hor2Label.setText(board.Hor2)
+    // this.Hor3Label.setText(board.Hor3)
+    // this.Hor4Label.setText(board.Hor4)
+    // this.Hor5Label.setText(board.Hor5)
+    // this.Hor6Label.setText(board.Hor6)
+    // this.Hor7Label.setText(board.Hor7)
+    // this.Hor8Label.setText(board.Hor8)
+    // this.Hor9Label.setText(board.Hor9)
   }
-  checkSolution() {
+
+  getTile(input) {
+    var xPos = input.downX
+    var yPos = input.downY
+    //console.log(xPos, yPos)
+
+    this.inputVert = Math.floor((yPos - 20) / 40)
+    // console.log(this.inputVert)
+
+    var horNames = ["Hor1", "Hor2", "Hor3", "Hor4", "Hor5", "Hor6", "Hor7", "Hor8", "Hor9"]
+    var inputHor = Math.floor((xPos - 170) / 40)
+    this.inputHorName = horNames[inputHor]
+    // console.log(this.inputVertName)
+
+    console.log(this.gameBoard[this.inputHorName][this.inputVert])
+
 
   }
 }
@@ -134,7 +156,7 @@ new Phaser.Game({
   height: 400, // Height of the game in pixels
   backgroundColor: '#919191', // The background color (grey)
   scene: mainScene, // The name of the scene we created
-  physics: { default: 'arcade', arcade: { debug: false } }, // The physics engine to use
+  physics: { default: 'arcade', arcade: { debug: true } }, // The physics engine to use
   parent: 'game', // Create the game inside the <div id="game"> 
 });
 
