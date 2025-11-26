bf8aa3e2aa11be9a07ec257f46253bf4c780a7e9
Tijnniebeer <tijn99@gmail.com>
2025-11-26 16:41:57 +0100

    Start dinogame Tijn

--- t_dinoGame/game.js
+++ t_dinoGame/game.js
@@ -3,39 +3,18 @@ class mainScene {
   // The three methods currently empty
 
   preload() {
-    /*     
-    This method is called once at the beginning
-    It will load all the assets, like sprites and sounds
-    */
+    this.load.image('ground', 't_dinoGame/assets/ground.png')
+  
   }
 
   create() {
-    /* 
-    This method is called once, just after preload()
-    It will initialize our scene, like the positions of the sprites
-    */
+    this.ground = this.add.tileSprite(0, 350, 1400, 100, 'ground')
 
   }
   update() {
-    /* 
-    This method is called 60 times per second after create() 
-    It will handle all the game's logic, like movements
-    */
 
-    /* Handle horizontal and vertical movements of "this.player"
-    if (this.arrow.right.isDown) {
-      // If the right arrow is pressed, move to the right
-      this.player.x += 3;
-    } else if (this.arrow.left.isDown) {
-      // If the left arrow is pressed, move to the left
-      this.player.x -= 3;
-    }
-    if (this.arrow.down.isDown) {
-      this.player.y += 3;
-    } else if (this.arrow.up.isDown) {
-      this.player.y -= 3;
-    }
-    */
+    this.ground.tilePositionX += 5
+    
 
   }
 
@@ -47,7 +26,7 @@ class mainScene {
 new Phaser.Game({
   width: 700, // Width of the game in pixels
   height: 400, // Height of the game in pixels
-  backgroundColor: '#919191ff', // The background color (grey)
+  backgroundColor: '#dba101', // The background color (grey)
   scene: mainScene, // The name of the scene we created
   physics: { default: 'arcade' }, // The physics engine to use
   parent: 'game', // Create the game inside the <div id="game"> 
