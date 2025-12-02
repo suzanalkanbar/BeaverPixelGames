a047d9ff9f84ee17e0411b05f43e33ce01776dd3
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-12-02 09:37:36 +0100

    tetris next, hold, score

--- website/sj_tetris/game.js
+++ website/sj_tetris/game.js
@@ -39,11 +39,21 @@ class mainScene {
 
     // next
     this.next = this.add.rectangle(575, 150, 100, 200, blackColor, 1)
+    this.add.text(575, 65, "Next:", { fontSize: 28, color: 'white' }).setOrigin(0.5, 0.5)
 
     // hold
+    this.next = this.add.rectangle(125, 125, 100, 150, blackColor, 1)
+    this.add.text(127, 65, "Hold:", { fontSize: 28, color: 'white' }).setOrigin(0.5, 0.5)
 
     // score
+    this.next = this.add.rectangle(125, 300, 225, 150, blackColor, 1)
+    this.scoreLabel = this.add.text(25, 235, "Score: 000,000", { fontSize: 24, color: 'white' })
+    this.levelLabel = this.add.text(25, 285, "Level: 0", { fontSize: 24, color: 'white' })
+    this.linesLabel = this.add.text(25, 335, "Lines: 0", { fontSize: 24, color: 'white' })
 
+    this.score = 0
+    this.level = 0
+    this.lines = 0
 
   }
 
@@ -52,7 +62,20 @@ class mainScene {
   }
 
   /* VVV Put any other functions and code down here VVV */
+  increaseScore(points) {
+    this.score += points
+    this.scoreLabel.setText("Score: " + this.score)
+  }
 
+  increaseLevel() {
+    this.level += 1
+    this.levelLabel.setText("Level: " + this.level)
+  }
+
+  increaseLines(numbLines) {
+    this.lines += numbLines
+    this.linesLabel.setText("Lines: " + this.lines)
+  }
 }
 
 // Create the game
