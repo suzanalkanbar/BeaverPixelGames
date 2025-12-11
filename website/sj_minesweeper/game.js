1e9df0c53dc98a33a93c2b907804bf6e9fa31f57
Sasja Driesenaar <driesenaar.sasja@gmail.com>
2025-12-11 11:42:13 +0100

    minesweeper sweep

--- website/sj_minesweeper/game.js
+++ website/sj_minesweeper/game.js
@@ -15,34 +15,115 @@ class mainScene {
     this.bgcolour = 0xced25e
 
     // constants
-    this.tilesize = 32
-    this.rows = 15
-    this.cols = 15
-    this.amount_mines = 5 // 30 in normal game
-    this.witdh = this.tilesize * this.rows
-    this.height = this.tilesize * this.cols
+    this.screenWidth = 700
+    this.screenHeight = 400
 
-    this.tile_numbers = []
-    for (var i = 1; i < 9; i++) {
-      tile_nu
-    }
+    this.tilescale = 1.6
+    this.tilesize = 16 * this.tilescale // original img is 16x16 px
+    this.rowsX = 15
+    this.colsY = 15
+    this.amount_mines = 5 // 30 in normal game
+    this.witdh = this.tilesize * this.rowsX
+    this.height = this.tilesize * this.colsY
   }
 
   preload() {
+    this.load.image('Tile1', 'sj_minesweeper/assets/Tile1.png')
+    this.load.image('Tile2', 'sj_minesweeper/assets/Tile2.png')
+    this.load.image('Tile3', 'sj_minesweeper/assets/Tile3.png')
+    this.load.image('Tile4', 'sj_minesweeper/assets/Tile4.png')
+    this.load.image('Tile5', 'sj_minesweeper/assets/Tile5.png')
+    this.load.image('Tile6', 'sj_minesweeper/assets/Tile6.png')
+    this.load.image('Tile7', 'sj_minesweeper/assets/Tile7.png')
+    this.load.image('Tile8', 'sj_minesweeper/assets/Tile8.png')
 
+    this.load.image('TileEmpty', 'sj_minesweeper/assets/TileEmpty.png')
+    this.load.image('TileExploded', 'sj_minesweeper/assets/TileExploded.png')
+    this.load.image('TileFlag', 'sj_minesweeper/assets/TileFlag.png')
+    this.load.image('TileMine', 'sj_minesweeper/assets/TileMine.png')
+    this.load.image('TileNotMine', 'sj_minesweeper/assets/TileNotMine.png')
+    this.load.image('TileUnknown', 'sj_minesweeper/assets/TileUnknown.png')
   }
 
   create() {
 
+    // tile numbers (makkelijker te gebruiken)
+    this.tile_numbers = ['Tile1', 'Tile2', 'Tile3', 'Tile4', 'Tile5', 'Tile6', 'Tile7', 'Tile8']
+    this.TileEmpty = 'TileEmpty'
+    this.TileExploded = 'TileExploded'
+    this.TileFlag = 'TileFlag'
+    this.TileMine = 'TileMine'
+    this.TileNotMine = 'TileNotMine'
+    this.TileUnknown = 'TileUnknown'
 
+    // add interactive square
+    this.interactiveBoard = this.add.rectangle(
+      (this.screenWidth / 2 - this.witdh / 2 + this.tilesize / 2),
+      (this.screenHeight / 2 - this.height / 2),
+      this.witdh, this.height, this.white, 1).setOrigin(0, 0)
+    this.physics.add.existing(this.interactiveBoard).setInteractive().on('pointerdown', this.getTile, this)
+
+    this.gameGrid = []
+    this.createGrid()
+
+    // voor single console log :3 as a treat
+    this.n = 0
   }
+
   update() {
 
-
+    if (this.n == 0) {
+      console.log(this.gameGrid)
+    }
+    this.n = 1
   }
 
   /* VVV Put any other functions and code down here VVV */
+  createTile(object, x, y, imgName, type) {
+    // Types list for grid:
+    //    '.' -> unknown
+    //    'X' -> mine
+    //    'C' -> clue
+    //    '/' -> empty 
+    object = this.add.image(0, 0, imgName).setScale(this.tilescale)
+    object.x = this.tilesize * x + (this.screenWidth / 2 - this.witdh / 2 + this.tilesize)
+    object.y = this.tilesize * y + (this.screenHeight / 2 - this.height / 2 + this.tilesize / 2)
+    object.type = type
 
+    return object
+  }
+
+  createGrid() {
+    for (var n = 0; n < this.rowsX; n++) {
+
+      for (var i = 0; i < this.colsY; i++) {
+        this.currentTile
+        this.newTile = this.createTile(this.currentTile, n, i, this.tile_numbers[0], '.')
+
+        this.gameGrid.push(this.newTile)
+      }
+    }
+  }
+
+  getTile(input) {
+    var xPos = input.downX
+    var yPos = input.downY
+
+    var inputX = Math.floor((xPos - (((this.screenWidth - this.witdh) / 2) + this.tilesize / 2)) / this.tilesize)
+    var inputY = Math.floor((yPos - ((this.screenHeight - this.height) / 2)) / this.tilesize)
+
+    var currentIndex = inputY * this.colsY + inputX
+    var currentTile = this.gameGrid[currentIndex]
+
+    console.log(currentTile)
+
+    // if (currentTile != 0) {
+    //     this.sound.play('missClick')
+    //   } else {
+    //     this.sound.play('tileClick')
+    //     this.changeNumber(inputRowName, inputX)
+    //   }
+  }
 }
 
 // Create the game
